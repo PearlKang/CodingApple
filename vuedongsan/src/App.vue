db1f447816fec6d7aa41902277e303f3d08dfc80
PearlKang <rksk1588@gmail.com>
2022-05-06 00:20:57 +0900

    2022.05.06

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  안녕하세요.
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h4 class="red" :style="스타일">{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price3 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      price1: 80,
+      price2: 70,
+      price3: "아무거나",
+      스타일: "color: blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
   },
+  components: {},
 };
 </script>
 
