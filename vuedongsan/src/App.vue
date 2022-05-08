cff8bc1775e773f86dee58510509d6a88a232e7d
PearlKang <rksk1588@gmail.com>
2022-05-08 16:53:38 +0900

    2022.05.08

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    test input
+    <div class="menu">
+      <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+    </div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <div>
-      <h4 class="red" :style="스타일">{{ products[0] }}</h4>
+      <h4>{{ products[0] }}</h4>
       <p>{{ price1 }} 만원</p>
     </div>
     <div>
@@ -22,10 +24,10 @@ export default {
   name: "App",
   data() {
     return {
+      메뉴들: ["Home", "Shop", "About"],
       price1: 80,
       price2: 70,
       price3: "아무거나",
-      스타일: "color: blue",
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
   },
@@ -41,5 +43,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
