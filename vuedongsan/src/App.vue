3ad6c80677d72c6ba03f6811cad94816ef92175f
PearlKang <rksk1588@gmail.com>
2022-05-11 01:11:39 +0900

    2022.05.11

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
+    <!-- modal -->
+    <div class="black-bg" v-if="modalStatus">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용</p>
+        <button @click="modalStatus = false">닫기</button>
+      </div>
+    </div>
+
+    <!-- top nav -->
     <div class="menu">
       <a v-for="i in menus" :key="i">{{ i }}</a>
     </div>
+
+    <!-- main lists -->
     <div v-for="(a, i) in 3" :key="i">
-      <h4>{{ products[i] }}</h4>
+      <img src="./assets/room0.jpg" class="room-img" />
+      <h4 @click="modalStatus = true">{{ products[i] }}</h4>
       <p>{{ prices[i] }} 만원</p>
       <button @click="reports[i]++">허위매물신고</button>
       <span>신고수 : {{ reports[i] }}</span>
@@ -21,14 +34,42 @@ export default {
       prices: [80, 70, "아무거나"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       reports: [0, 0, 0],
+      modalStatus: false,
     };
   },
-  methods: {},
+  methods: {
+    increase() {
+      this.reports++;
+    },
+  },
   components: {},
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -46,5 +87,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
