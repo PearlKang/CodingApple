e57447fcb2bd081eeb958a490be9eb33f3b5a1d9
PearlKang <rksk1588@gmail.com>
2022-05-23 00:29:03 +0900

    2022.05.23

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
     <!-- modal -->
-    <!-- <div class="black-bg" v-if="modalStatus">
-      <div class="white-bg">
-        <img :src="data[selectItem].image" class="room-img-madal" />
-        <h4>{{ data[selectItem].title }}</h4>
-        <p>{{ data[selectItem].content }}</p>
-        <p>{{ data[selectItem].price }} 원</p>
-        <button @click="modalStatus = false">닫기</button>
-      </div>
-    </div> -->
-
     <Modal :data="data" :selectItem="selectItem" :modalStatus="modalStatus" />
 
     <!-- top nav -->
