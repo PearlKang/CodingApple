ceb3a70c9b718f88c32399d8be9ad72efdd04432
PearlKang <rksk1588@gmail.com>
2022-06-16 00:46:53 +0900

    2022.06.16

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from "./Post";
+
+export default {
+  name: "Container",
+  components: { Post },
+};
+</script>
+
+<style></style>
