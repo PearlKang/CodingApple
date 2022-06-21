ac99d63684657683da54b369b5120bacd064546e
PearlKang <rksk1588@gmail.com>
2022-06-22 00:19:25 +0900

    2022.06.22

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -9,7 +9,7 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :data="data" />
+  <Container :data="data" :step="step" />
 
   <button @click="more">더보기</button>
 
@@ -19,13 +19,6 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
-
-  <div v-if="step == 0">내용0</div>
-  <div v-if="step == 1">내용1</div>
-  <div v-if="step == 2">내용2</div>
-  <button @click="step = 0">버튼0</button>
-  <button @click="step = 1">버튼1</button>
-  <button @click="step = 2">버튼2</button>
 </template>
 
 <script>
--- vuestagram/src/components/Container.vue
+++ vuestagram/src/components/Container.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <Post :data="a" v-for="(a, i) in data" :key="i" />
+    <div v-if="step == 0">
+      <Post :data="a" v-for="(a, i) in data" :key="i" />
+    </div>
+
+    <div v-if="step == 1">
+      <div class="upload-image"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+
+    <div v-if="step == 2">
+      <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,8 +32,52 @@ export default {
   components: { Post },
   props: {
     data: Array,
+    step: Number,
   },
 };
 </script>
 
-<style></style>
+<style>
+.upload-image {
+  width: 100%;
+  height: 450px;
+  background: cornflowerblue;
+  background-size: cover;
+}
+.filters {
+  overflow-x: scroll;
+  white-space: nowrap;
+}
+.filter-1 {
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+}
+.filters::-webkit-scrollbar {
+  height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.write-box {
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
+}
+</style>
