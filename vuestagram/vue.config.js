3dea442f1301c81f962cf907620c593886123898
PearlKang <rksk1588@gmail.com>
2022-07-03 17:14:04 +0900

    2022.07.03

--- vuestagram/vue.config.js
+++ vuestagram/vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  pwa: {
+    name: "Vuestagram",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    workboxOptions: {
+      exclude: [/\.map$/, /manifest\.json$/, "index.html"],
+    },
+  },
 });
