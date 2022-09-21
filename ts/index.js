617244b0ddb03482bf45ed9624766d73d338d979
BenKang <rksk1588@gmail.com>
2022-09-22 00:49:07 +0900

    2022.09.22

--- ts/index.js
+++ ts/index.js
@@ -17,3 +17,34 @@ var User = /** @class */ (function () {
     }
     return User;
 }());
+var name1 = "kim";
+var age1 = 50;
+var isMarried1 = true;
+var isMarried2 = null;
+var isMarried3 = undefined;
+var members1 = ["kim", "park"];
+var members11 = ["kim", 123, true];
+var members2 = {
+    member1: "kim",
+    member2: "park",
+};
+var member3 = "park";
+var myInfo1 = { name: "ben", age: 35, hometown: "busan" };
+var myInfo2 = {
+    name: "ben",
+    age: 35,
+    hometown: "busan",
+};
+var myFavorite1 = {
+    song: "title",
+    singer: "singer",
+};
+var myFavorite2 = {
+    song: "title",
+    singer: "singer",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
--- ts/index.ts
+++ ts/index.ts
@@ -1,14 +1,14 @@
-const nameStr1: string = "kim";
-const nameStrArray1: string[] = ["kim", "park"];
-const nameObject1: { name: string } = { name: "kim" };
-const nameObject2: { name?: string } = {};
+let nameStr1: string = "kim";
+let nameStrArray1: string[] = ["kim", "park"];
+let nameObject1: { name: string } = { name: "kim" };
+let nameObject2: { name?: string } = {};
 
-const nameStr2: string | number = "kim";
-const nameStr3: string[] | number = 123;
+let nameStr2: string | number = "kim";
+let nameStr3: string[] | number = 123;
 
 type Name = string | number;
 
-const nameStr4: Name = 123;
+let nameStr4: Name = 123;
 
 function func(x: number): number {
   return x * 2;
@@ -16,19 +16,19 @@ function func(x: number): number {
 
 type Member1 = [number, boolean];
 
-const john1: Member1 = [123, true];
+let john1: Member1 = [123, true];
 
 type Member2 = {
   name: string;
 };
 
-const john2: Member2 = { name: "kim" };
+let john2: Member2 = { name: "kim" };
 
 type Member3 = {
   [key: string]: string;
 };
 
-const john3: Member3 = { name: "kim", age: "12" };
+let john3: Member3 = { name: "kim", age: "12" };
 
 class User {
   name: string;
@@ -36,3 +36,41 @@ class User {
     this.name = name;
   }
 }
+
+let name1: string = "kim";
+let age1: number = 50;
+let isMarried1: boolean = true;
+
+let isMarried2: null = null;
+let isMarried3: undefined = undefined;
+
+let members1: string[] = ["kim", "park"];
+let members11: (string | number | boolean)[] = ["kim", 123, true];
+let members2: { member1: string; member2: string } = {
+  member1: "kim",
+  member2: "park",
+};
+
+let member3 = "park";
+
+let myInfo1 = { name: "ben", age: 35, hometown: "busan" };
+let myInfo2: { name: string; age: number; hometown: string } = {
+  name: "ben",
+  age: 35,
+  hometown: "busan",
+};
+
+let myFavorite1 = {
+  song: "title",
+  singer: "singer",
+};
+let myFavorite2: { song: string; singer: string } = {
+  song: "title",
+  singer: "singer",
+};
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
