fcbaaedbd1d4e36d15d750a9afd9fa2626aa6dbe
BenKang <rksk1588@gmail.com>
2022-11-03 01:04:54 +0900

    2022.11.03

--- basic-python/파이썬장인의폴더/app.py
+++ basic-python/파이썬장인의폴더/app.py
@@ -1,1 +1,19 @@
-print("안녕")
+print(1111)
+print(123+123)
+
+print("차은우"*10)
+
+print("슈퍼 에이전트 하이퍼 초필살 드래곤")
+print("슈퍼 에이전트 하이퍼 초필살 드래곤")
+print("슈퍼 에이전트 하이퍼 초필살 드래곤")
+print("슈퍼 에이전트 하이퍼 초필살 드래곤")
+
+# >
+print()
+# >
+
+name="슈퍼 에이전트 하이퍼 초필살 드래곤"
+
+print(name)
+print(name[0])  # indexing 인덱싱
+print(name[0:2])
