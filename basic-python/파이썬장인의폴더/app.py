6cf7545cc80db9d3b1c873b5ae50cb5b9b3b5351
BenKang <rksk1588@gmail.com>
2022-11-04 00:59:48 +0900

    2022.11.04

--- basic-python/파이썬장인의폴더/app.py
+++ basic-python/파이썬장인의폴더/app.py
@@ -17,3 +17,34 @@ name="슈퍼 에이전트 하이퍼 초필살 드래곤"
 print(name)
 print(name[0])  # indexing 인덱싱
 print(name[0:2])
+
+# array, 배열
+중고차1="K5 white 5000"
+print(중고차1)
+
+중고차2=["K5","white",5000]
+print(중고차2)
+print(중고차2[0])
+print(중고차2[1])
+
+중고차2[1]="black"
+print(중고차2[1])
+
+# 중고차2.sort()
+# 중고차2.reverse()
+# 중고차2.pop()
+
+중고차3=["K5","white",[5000,6000]]
+print(중고차3)
+
+# dictionary, object
+중고차4={"brand":"BMW","model":"520D"}
+중고차4={
+    "brand":"BMW",
+    "model":"520D"
+}
+print(중고차4)
+print(중고차4["brand"])
+
+중고차4["brand"]=1234
+print(중고차4["brand"])
